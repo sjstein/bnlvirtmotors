73cc2a569e7405a0fe5338f59f5f2b40083bb674
sjstein <s.joshua.stein@gmail.com>
2022-07-20 20:39:19 +0000

    changes for coordinated relative motion

--- iocBoot/ioc-virtmotors/st.cmd
+++ iocBoot/ioc-virtmotors/st.cmd
@@ -1,13 +1,16 @@
 #!../../bin/linux-x86_64/virtMotors
 
 < envPaths
-< /epics/common/xf19id2-ioc1-netsetup.cmd
+# Below may not be needed for local instance
+#< /epics/common/xf19id2-ioc1-netsetup.cmd
 
 # Name of the DCM Energy virtual motor, and of related real motors
 epicsEnvSet("ENGY",      "XF:19IDC-OP{Mono:DCM-Ax:E}Mtr")
+#epicsEnvSet("ENGY",      "EnergyMotor")
 epicsEnvSet("BRAGGMTR",  "XF:19IDC-OP{Mono:DCM-Ax:B}Mtr")
-epicsEnvSet("PERPMTR",   "XF:19IDC-OP{Mono:DCM-Ax:Perp}Mtr")
-epicsEnvSet("PARAMTR",   "XF:19IDC-OP{Mono:DCM-Ax:Para}Mtr")
+#epicsEnvSet("BRAGGMTR",  "IOC:m4")
+epicsEnvSet("PERPMTR",   "PerpMotor")
+epicsEnvSet("PARAMTR",   "ParaMotor")
 
 # Name of the DCM 2nd Crystal Bend virtual motor, and of the two real motors
 epicsEnvSet("C2BND",     "XF:19IDC-OP{Mono:DCM-Ax:C2Bnd}Mtr")
@@ -27,16 +30,18 @@ epicsEnvSet("FRMTR",     "XF:19IDC-CT{DIODE-Local:3}OutCh01:Data-SP")
 dbLoadDatabase("$(TOP)/dbd/virtMotors.dbd",0,0)
 virtMotors_registerRecordDeviceDriver(pdbbase)
 
-dbLoadRecords("$(TOP)/db/energy.template",      "P=$(ENGY),BRAGGMTR=$(BRAGGMTR),PERPMTR=$(PERPMTR),PARAMTR=$(PARAMTR)")
+#dbLoadRecords("$(TOP)/db/energy.template",      "P=$(ENGY),BRAGGMTR=$(BRAGGMTR),PERPMTR=$(PERPMTR),PARAMTR=$(PARAMTR)")
 dbLoadRecords("$(TOP)/db/dcm_c2_bend.template", "P=$(C2BND),C2BND1MTR=$(C2BND1MTR),C2BND2MTR=$(C2BND2MTR)")
-dbLoadRecords("$(TOP)/db/dcm_piezo.template",   "P=$(FP),DESC=$(FPDESC),MTR=$(FPMTR)")
-dbLoadRecords("$(TOP)/db/dcm_piezo.template",   "P=$(FR),DESC=$(FRDESC),MTR=$(FRMTR)")
+#dbLoadRecords("$(TOP)/db/dcm_piezo.template",   "P=$(FP),DESC=$(FPDESC),MTR=$(FPMTR)")
+#dbLoadRecords("$(TOP)/db/dcm_piezo.template",   "P=$(FR),DESC=$(FRDESC),MTR=$(FRMTR)")
 
-dbLoadRecords("$(IOCSTATS)/db/iocAdminSoft.db", "IOC=XF:19IDC-OP{VirtMotors}")
+#dbLoadRecords("$(IOCSTATS)/db/iocAdminSoft.db", "IOC=XF:19IDC-OP{VirtMotors}")
 
 iocInit()
 
 dbpf $(C2BND).SYNC 1
-dbpf $(ENGY).SYNC 1
-dbpf $(FP).SYNC 1
-dbpf $(FR).SYNC 1
+#dbpf $(C2BND1MTR)-TwkPos.TPRO 1
+#dbpf $(C2BND).TWV .2
+#dbpf $(ENGY).SYNC 1
+#dbpf $(FP).SYNC 1
+#dbpf $(FR).SYNC 1
